a0334d944d665cb35df4500639e80428b8de1d84
tmsblzs <tmsblzs+github@gmail.com>
2022-01-08 20:32:05 +0100

    Szamla agent connector's setting and invoice doctypes are created.

---
+++ szamlazz_agent_connector/szamlazz_agent_connector/doctype/szamlazzagentconnectorsetting/szamlazzagentconnectorsetting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, coding.pensav.hu and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SzamlazzAgentConnectorSetting', {
+	// refresh: function(frm) {
+
+	// }
+});
