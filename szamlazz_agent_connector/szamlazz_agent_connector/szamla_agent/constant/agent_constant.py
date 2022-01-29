84c50227e948709c7d967dd25585da0b3177dab9
tmsblzs <tmsblzs+github@gmail.com>
2022-01-29 23:12:17 +0100

    Saving SzamlazzAgentConnectorInvoice

--- szamlazz_agent_connector/szamlazz_agent_connector/szamla_agent/constant/agent_constant.py
+++ szamlazz_agent_connector/szamlazz_agent_connector/szamla_agent/constant/agent_constant.py
@@ -5,6 +5,6 @@ class AgentConstant:
     COOKIE_FILENAME = 'cookie/cookie.txt'
     CERTIFICATION_FILENAME = 'cacert.pem'
     CERTIFICATION_PATH = 'cert/'
-    PDF_FILE_SAVE_PATH = 'pdf/'
-    XML_FILE_SAVE_PATH = 'xmls/'
+    PDF_FILE_SAVE_PATH = '.'
+    XML_FILE_SAVE_PATH = '.'
     ATTACHMENTS_SAVE_PATH = './attachments'
