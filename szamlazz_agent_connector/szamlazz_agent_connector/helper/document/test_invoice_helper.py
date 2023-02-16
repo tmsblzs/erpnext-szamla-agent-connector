fcc69da2c4d7cd166459afffd7c4339082c8f55a
tmsblzs <tmsblzs+github@gmail.com>
2023-02-16 19:53:24 +0100

    Adjust test_invoice_helper.

--- szamlazz_agent_connector/szamlazz_agent_connector/helper/document/test_invoice_helper.py
+++ szamlazz_agent_connector/szamlazz_agent_connector/helper/document/test_invoice_helper.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 import frappe
 from frappe.tests.utils import FrappeTestCase
 from szamlazz_agent_connector.szamlazz_agent_connector.helper.document.invoice_helper import InvoiceHelper
+from szamlazz_agent_connector.szamlazz_agent_connector.model.constant.invoice_constant import InvoiceConstant
 from szamlazz_agent_connector.szamlazz_agent_connector.szamla_agent.buyer import Buyer
 from szamlazz_agent_connector.szamlazz_agent_connector.szamla_agent.seller import Seller
 
@@ -68,3 +69,53 @@ class TestInvoiceHelper(FrappeTestCase):
         InvoiceHelper.create_from_sales_invoice(sales_invoice)
 
         assert sales_items_helper.add_items_to_invoice.called
+
+    def test_fill_invoice_the_right_seller(
+            self,
+            buyer_helper,
+            seller_helper,
+            invoice_header_helper,
+            sales_items_helper):
+        seller = self._setup_seller_helper(seller_helper)
+        buyer = Buyer
+        buyer_helper.get_buyer_from_sales_invoice.return_value = buyer
+        sales_invoice = frappe.new_doc("Sales Invoice")
+
+        invoice = InvoiceHelper.create_from_sales_invoice(sales_invoice)
+
+        self.assertEqual(invoice.seller, seller)
+
+    def _setup_seller_helper(self, seller_helper):
+        seller = Seller
+        seller_helper.get_from_sales_invoice.return_value = seller
+        return seller
+
+    def test_fill_invoice_the_right_buyer(
+            self,
+            buyer_helper,
+            seller_helper,
+            invoice_header_helper,
+            sales_items_helper):
+        seller = self._setup_seller_helper(seller_helper)
+        buyer = Buyer
+        buyer_helper.get_buyer_from_sales_invoice.return_value = buyer
+        sales_invoice = frappe.new_doc("Sales Invoice")
+
+        invoice = InvoiceHelper.create_from_sales_invoice(sales_invoice)
+
+        self.assertEqual(invoice.buyer, buyer)
+
+    def test_header_type_is_P_INVOICE(
+            self,
+            buyer_helper,
+            seller_helper,
+            invoice_header_helper,
+            sales_items_helper):
+        seller = self._setup_seller_helper(seller_helper)
+        buyer = Buyer
+        buyer_helper.get_buyer_from_sales_invoice.return_value = buyer
+        sales_invoice = frappe.new_doc("Sales Invoice")
+
+        invoice = InvoiceHelper.create_from_sales_invoice(sales_invoice)
+
+        self.assertEqual(invoice.header.invoice_type, InvoiceConstant.INVOICE_TYPE_P_INVOICE)
--- szamlazz_agent_connector/szamlazz_agent_connector/helper/participant/seller_helper.py
+++ szamlazz_agent_connector/szamlazz_agent_connector/helper/participant/seller_helper.py
@@ -6,12 +6,12 @@ class SellerHelper:
     @staticmethod
     def get_from_sales_invoice(sales_invoice):
         company_name = sales_invoice.company
-        account = SellerHelper.get_account_or_default(company_name)
+        account = SellerHelper._get_account_or_default(company_name)
         seller = Seller(account['bank'], account['account_no'])
         return seller
 
     @staticmethod
-    def get_account_or_default(company_name):
+    def _get_account_or_default(company_name):
         company = frappe.get_doc('Company', company_name)
         bank_accounts = frappe.get_all('Bank Account', filters={'account': company.default_bank_account})
         account = {'bank': "", 'account_no': ""}
