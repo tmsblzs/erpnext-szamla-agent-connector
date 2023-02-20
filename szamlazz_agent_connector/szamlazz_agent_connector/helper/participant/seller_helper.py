38460d509107ae5e693992739873714d5fc5228e
tmsblzs <tmsblzs+github@gmail.com>
2023-02-20 23:06:39 +0100

    Add test_seller_helper.

--- szamlazz_agent_connector/szamlazz_agent_connector/helper/participant/seller_helper.py
+++ szamlazz_agent_connector/szamlazz_agent_connector/helper/participant/seller_helper.py
@@ -1,4 +1,5 @@
 import frappe
+from szamlazz_agent_connector.szamlazz_agent_connector.helper.account_helper import AccountHelper
 from szamlazz_agent_connector.szamlazz_agent_connector.model.participant.seller import Seller
 
 
@@ -6,18 +7,8 @@ class SellerHelper:
     @staticmethod
     def get_from_sales_invoice(sales_invoice):
         company_name = sales_invoice.company
-        account = SellerHelper._get_account_or_default(company_name)
+        company = frappe.get_doc('Company', company_name)
+        account = AccountHelper.get_account_or_default(company)
         seller = Seller(account['bank'], account['account_no'])
         return seller
 
-    @staticmethod
-    def _get_account_or_default(company_name):
-        company = frappe.get_doc('Company', company_name)
-        bank_accounts = frappe.get_all('Bank Account', filters={'account': company.default_bank_account})
-        account = {'bank': "", 'account_no': ""}
-        if bank_accounts:
-            bank_account = frappe.get_doc('Bank Account', bank_accounts[0])
-            if bank_account:
-                account['bank'] = bank_account.bank
-                account['account_no'] = bank_account.bank_account_no
-        return account
